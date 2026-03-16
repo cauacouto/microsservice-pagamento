b912a7c90ea98c0915f271f110e1df3dea5f6583
caua <cauacouto022@gmail.com>
2026-03-15 21:34:03 -0300

    feat : create itemDoPedido

---
+++ src/main/java/br/com/alurafood/pagamentos/model/ItemDoPedido.java
@@ -0,0 +1,38 @@
+package br.com.alurafood.pagamentos.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemDoPedido {
+
+
+    private Long id;
+    private Integer quantidade;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
