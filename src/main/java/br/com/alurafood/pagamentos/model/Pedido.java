5d091f26597440f440c9754d9d498ba1525cabc5
caua <cauacouto022@gmail.com>
2026-03-15 21:31:37 -0300

    feat: create entity pedido

---
+++ src/main/java/br/com/alurafood/pagamentos/model/Pedido.java
@@ -0,0 +1,17 @@
+package br.com.alurafood.pagamentos.model;
+
+import java.util.List;
+
+
+public class Pedido {
+
+    private List<ItemDoPedido> itens;
+
+    public List<ItemDoPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemDoPedido> itens) {
+        this.itens = itens;
+    }
+}
