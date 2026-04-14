5d5b11ffbb394235f8a3cd4f813df54a81bdf589
cauacouto <cauacouto022@gmail.com>
2026-04-13 21:57:49 -0300

    update:retirar fila pagamento

--- src/main/java/br/com/alurafood/pagamentos/AmqpConfigure/PagamentoConfigurationAmqp.java
+++ src/main/java/br/com/alurafood/pagamentos/AmqpConfigure/PagamentoConfigurationAmqp.java
@@ -1,7 +1,6 @@
 package br.com.alurafood.pagamentos.AmqpConfigure;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -14,10 +13,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PagamentoConfigurationAmqp {
 
-@Bean
-    public Queue criarFila(){
-        return QueueBuilder.nonDurable("pagamento.concluido").build();
-    }
 
     @Bean
     public RabbitAdmin criarRabbitAdmin(ConnectionFactory conn){
@@ -41,5 +36,9 @@ public class PagamentoConfigurationAmqp {
     RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
     rabbitTemplate.setMessageConverter(messageConverter);
     return rabbitTemplate;
+}
+@Bean
+public FanoutExchange fanoutExchange(){
+        return new FanoutExchange("pagamentos.ex");
 }
 }
